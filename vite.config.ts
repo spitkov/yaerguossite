017172490536a4afdce3c2a2b8619633f7d0f29b
spitkov <github@spitkov.hu>
2025-01-05 13:25:46 +0100

    mabye no gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		target: 'esnext',
-		assetsDir: '_app',
-		rollupOptions: {
-			output: {
-				manualChunks: undefined
-			}
-		}
-	},
-	server: {
-		fs: {
-			allow: ['..']
-		}
-	}
+	plugins: [sveltekit()]
 });
