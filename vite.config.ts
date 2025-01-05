19cfc8068d447b25a9b31c3d55ac2845af2d92c2
spitkov <github@spitkov.hu>
2025-01-05 13:23:33 +0100

    ssr

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		target: 'esnext',
+		assetsDir: '_app',
+		rollupOptions: {
+			output: {
+				manualChunks: undefined
+			}
+		}
+	},
+	server: {
+		fs: {
+			allow: ['..']
+		}
+	}
 });
